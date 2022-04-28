787919695151b93eebdc5c118230fd43f151145a
mtbguysupremo <jasonapr99@gmail.com>
2022-04-28 09:03:19 -0400

    added the random color function and clear board function! onto add the click to draw function.

--- index.js
+++ index.js
@@ -37,5 +37,19 @@ else {
 }
 
 function colorSquare() {
-    this.style.backgroundColor = color;
+    if(color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice){
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
 }
